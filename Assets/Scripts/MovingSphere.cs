fe2dadc0623c1914587af8f2c023f431a9d7835a
chenning01 <chenning01@bianfeng.com>
2022-04-23 23:49:33 +0800

    物体运动一

---
+++ Assets/Scripts/MovingSphere.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSphere : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+
+    [SerializeField]
+    Rect allowedArea = new Rect(-4.5f, -4.5f, 10f, 10f);
+
+    [SerializeField, Range(0f, 1f)]
+    float bounciness = 0.5f;
+
+    Vector3 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerInput = Vector3.zero;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.z = Input.GetAxis("Vertical");
+        playerInput = Vector3.ClampMagnitude(playerInput, 1f);
+
+        Vector3 desiredVelocity = new Vector3(playerInput.x, playerInput.y, playerInput.z) * maxSpeed; //期望速度
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+        Vector3 newPosition = transform.localPosition + velocity * Time.deltaTime;
+        if (newPosition.x < allowedArea.xMin)
+        {
+            newPosition.x = allowedArea.xMin;
+            velocity.x = -velocity.x * bounciness;
+        }
+        else if (newPosition.x > allowedArea.xMax)
+        {
+            newPosition.x = allowedArea.xMax;
+            velocity.x = -velocity.x * bounciness;
+        }
+        if (newPosition.z < allowedArea.yMin)
+        {
+            newPosition.z = allowedArea.yMin;
+            velocity.z = -velocity.z * bounciness;
+        }
+        else if (newPosition.z > allowedArea.yMax)
+        {
+            newPosition.z = allowedArea.yMax;
+            velocity.z = -velocity.z * bounciness;
+        }
+
+        //if (!allowedArea.Contains(new Vector2(newPosition.x, newPosition.z)))
+        //{
+        //    newPosition.x = Mathf.Clamp(newPosition.x, allowedArea.xMin, allowedArea.xMax);
+        //    newPosition.z = Mathf.Clamp(newPosition.z, allowedArea.yMin, allowedArea.yMax);
+        //}
+        transform.localPosition = newPosition;
+    }
+}
